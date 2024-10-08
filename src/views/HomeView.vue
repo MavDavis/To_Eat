174edd8cd5a52f58d805eca7fc316e509b41cfe4
Mavdavis <davidmarvelous@seamlesshr.com>
2024-10-08 22:44:07 +0100

    wip

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+const restaurantList = ref<string[]>([])
+const newRestaurantName = ref<string>('')
+const submitRestaurantName = () => {
+  restaurantList.value.push(newRestaurantName.value)
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home page</h1>
+    <form @submit.prevent="submitRestaurantName">
+      <input type="text" v-model="newRestaurantName" />
+      <button>submit</button>
+    </form>
+    <ul>
+      <li v-for="item in restaurantList" :key="item">{{ item }}</li>
+    </ul>
   </main>
 </template>
